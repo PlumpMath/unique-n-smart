12e87a8c78fa2b32eed4cc5928c77a994c8f14eb
root <root@droid>
2016-01-07 22:06:56 -0500

    make it better

---
+++ public/assets/frontend/onepage/scripts/translations.js
@@ -0,0 +1,337 @@
+function TranslateSite(lang) {
+    $('#title').html({'es': 'UNIQUE & SMART', 'en': 'UNIQUE & SMART'}[lang]);
+    $('.detail').html({
+        'es': "Detalles",
+        'en': "Details"
+    }[lang]);
+    $('.detail').html({
+        'es': "Detalles",
+        'en': "Details"
+    }[lang]);
+    $('#menu1').html({
+        'es': "Inicio",
+        'en': "Home"
+    }[lang]);
+    $('#menu2').html({
+        'es': "Nosotros",
+        'en': "About"
+    }[lang]);
+    $('#menu3').html({
+        'es': "Servicios",
+        'en': "Services"
+    }[lang]);
+    $('#menu4').html({
+        'es': "Equipo",
+        'en': "Team"
+    }[lang]);
+    $('#menu5').html({
+        'es': "Portafolio",
+        'en': "Portfolio"
+    }[lang]);
+    $('#menu6').html({
+        'es': "Beneficios",
+        'en': "Benefits"
+    }[lang]);
+    $('#menu7').html({
+        'es': "Precios",
+        'en': "Pricing"
+    }[lang]);
+    $('#menu8').html({
+        'es': "Contactos",
+        'en': "Contacts"
+    }[lang]);
+    $('#text1').html({'es': 'Manos a la obra', 'en': "Let's just do it"}[lang]);
+    $('#text2').html({
+        'es': "Cree su negocio en segundos con la<br> ayuda de nuestros<a href=\"javascript:void(0);\"> especialistas</a>",
+        'en': "Build your business in seconds<br> with our <a href=\"javascript:void(0);\">help</a>"
+    }[lang]);
+    $('#text3').html({
+        'es': "UNIQUE & SMART <span>Ha llegado</span>",
+        'en': "UNIQUE & SMART <span>Has Arrived</span>"
+    }[lang]);
+    $('#text4').html({
+        'es': "<span>aplicaciones</span> para moviles",
+        'en': "mobile<span> apps</span>"
+    }[lang]);
+    $('#text5').html({
+        'es': "La forma mas sencilla de entrar al Universo<br/>es atravesando bosques de adversidades",
+        'en': "The clearest way into the Universe<br/>is through a forest wilderness."
+    }[lang]);
+    $('#text6').html({
+        'es': "Droid Swalerzon",
+        'en': "Droid Swalerzon"
+    }[lang]);
+    $('#text7').html({
+        'es': "Construyendo negocios unicos inteligentemente",
+        'en': "Building unique business with smart ideas"
+    }[lang]);
+    $('#text8').html({
+        'es': "Codigo limpio",
+        'en': "Clean code"
+    }[lang]);
+    $('#text9').html({
+        'es': "Actualizaciones",
+        'en': "Huge Updates"
+    }[lang]);
+    $('#text10').html({
+        'es': "Adaptable",
+        'en': "Responsive"
+    }[lang]);
+    $('#text11').html({
+        'es': "Nuestros <strong>servicios</strong>",
+        'en': "OUR <strong>services</strong>"
+    }[lang]);
+    $('#text12').html({
+        'es': "En <strong>UNIQUE & SMART</strong> desarrollamos una amplia gama de aplicaciones web brindando servicios de calidad",
+        'en': "<strong>UNIQUE & SMART</strong> offers you a lot of web application defaults giving the client services of the higuest quality"
+    }[lang]);
+    $('#text13').html({
+        'es': "Soporte las 24 horas",
+        'en': "24 Hours support"
+    }[lang]);
+    $('#text14').html({
+        'es': "Contactenos via email o establezca conversaciones chat con nuestros especialistas",
+        'en': "Contact us by email or start a chat with our specialists"
+    }[lang]);
+    $('#text15').html({
+        'es': "Soluciones para moviles",
+        'en': "Mobile solutions"
+    }[lang]);
+    $('#text16').html({
+        'es': "Proporcionamos aplicaciones moviles a negocios que asi lo requieran",
+        'en': "We build mobile applications to business management"
+    }[lang]);
+    $('#text17').html({
+        'es': "Soporte para estadisticas",
+        'en': "Statistics support"
+    }[lang]);
+    $('#text18').html({
+        'es': "Desarrollamos aplicaciones con estadisticas y graficos",
+        'en': "We develop applications involving charts for business management"
+    }[lang]);
+    $('#text19').html({
+        'es': "Fotografia y disenno",
+        'en': "Design and photography"
+    }[lang]);
+    $('#text20').html({
+        'es': "Creamos disennos para el negocio y estudios",
+        'en': "Developing business visual design"
+    }[lang]);
+    $('#text21').html({
+        'es': "\"Dos cabezas piensan mejor que una\"<strong>dos cabezas inteligentes piensan aun mejor</strong>",
+        'en': "\"Two heads are better than one\"<strong>two smart heads are even better</strong>"
+    }[lang]);
+    $('#text22').html({
+        'es': "Ultimos <strong>trabajos</strong>",
+        'en': "Latest <strong>works</strong>"
+    }[lang]);
+    $('#w1').html({
+        'es': "JUST MOVE IT",
+        'en': "JUST MOVE IT"
+    }[lang]);
+    $('#w1e').html({
+        'es': "Juego",
+        'en': "Game"
+    }[lang]);
+    $('#w2').html({
+        'es': "Barbara Milian",
+        'en': "Barbara Milian"
+    }[lang]);
+    $('#w2e').html({
+        'es': "Sitio promocional",
+        'en': "Promotional site"
+    }[lang]);
+
+    $('#text23').html({
+        'es': "Por que <strong>escogernos?</strong>",
+        'en': "Why to <strong>choose us?</strong>"
+    }[lang]);
+    $('#text24').html({
+            'es': "A pesar de ser una empresa joven, tenemos una <a href=\"javascript:void(0);\">voluntad de acero</a> que nos mueve<br> nos inspira, nos hacer crecer. Trabajamos para contruir el futuro",
+            'en': "Besides we are a young group, we count with a <a href=\"javascript: void(0);\">steel will</a> that moves us<br>inspire our steps and help us to grown. We work building our future.",
+        }
+            [lang]
+    );
+    $('#text25').html({
+        'es': "Atencion al cliente",
+        'en': "Client support"
+    }[lang]);
+    $('#text26').html({
+        'es': "asd",
+        'en': ""
+    }[lang]);
+    $('#text27').html({
+        'es': "Mejores ofertas",
+        'en': ""
+    }[lang]);
+    $('#text28').html({
+        'es': "asd",
+        'en': ""
+    }[lang]);
+    $('#text29').html({
+        'es': "Precios modicos",
+        'en': ""
+    }[lang]);
+    $('#text30').html({
+        'es': "precios",
+        'en': ""
+    }[lang]);
+    $('#text31').html({
+        'es': "Desarrollo completo de negocio",
+        'en': ""
+    }[lang]);
+    $('#text32').html({
+        'es': "desarrollo",
+        'en': ""
+    }[lang]);
+    $('#text33').html({
+        'es': "Mantenimiento a las soluciones",
+        'en': ""
+    }[lang]);
+    $('#text34').html({
+        'es': "mantenimiento",
+        'en': ""
+    }[lang]);
+    $('#text35').html({
+        'es': "Ideas innovadoras",
+        'en': ""
+    }[lang]);
+    $('#text36').html({
+        'es': "innovacion",
+        'en': ""
+    }[lang]);
+    $('#text37').html({
+        'es': "Oferta especial! <em><br> contratenos ahora y ahorre 12% de su dinero en un proyecto</em>",
+        'en': "Especial offert!<em><br> hire us know and save 12% of your money in one project</em>"
+    }[lang]);
+    $('#text38').html({
+        'es': "Contratenos",
+        'en': "Contract us"
+    }[lang]);
+    $('#text39').html({
+        'es': "Algunos hechos sobre nosotros",
+        'en': "Some facts about us"
+    }[lang]);
+    $('#p1').html({
+        'es': "<strong>39</strong>Proyectos Completados",
+        'en': "<strong>39</strong>Projects Completed"
+    }[lang]);
+    $('#p2').html({
+        'es': "<strong>4</strong>Miembros del equipo",
+        'en': "<strong>4</strong>Team Members"
+    }[lang]);
+    $('#p3').html({
+        'es': "<strong>23</strong>Productos vendidos",
+        'en': "<strong>23</strong>Products sold"
+    }[lang]);
+    $('#text40').html({
+        'es': "Tabla de <strong>Precios</strong>",
+        'en': "<strong>Pricing</strong> Tables"
+    }[lang]);
+    $('#item1').html({
+        'es': "Blog",
+        'en': "Blog"
+    }[lang]);
+    $('#item1d').html({
+        'es': "Blog promocional utilizando bases de datos, integracion a redes sociales y mas",
+        'en': "Promotocional blog using data bases, social network integration and many more"
+    }[lang]);
+    $('#item1p').html({
+        'es': "<strong>$<em>150</em></strong><p>Precio inicial</p>",
+        'en': "<strong>$<em>150</em></strong><p>Starting price</p>"
+    }[lang]);
+    $('#item1li').html({
+        'es': "" +
+        "<li><i class=\"fa fa-circle\"></i> Autenticacion</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Redes sociales</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Posicionamiento online</li>",
+        'en': "" +
+        "<li><i class=\"fa fa-circle\"></i> Auth</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Social network</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Online positioning</li>",
+    }[lang]);
+    $('#item2').html({
+        'es': "Gestion",
+        'en': "Management"
+    }[lang]);
+    $('#item2d').html({
+        'es': "Sitio de gestion empresarial con soporte para multiples entidades en el negocio",
+        'en': "Management site with multiple business entities support"
+    }[lang]);
+    $('#item2p').html({
+        'es': "<strong>$<em>250</em></strong><p>Precio inicial</p>",
+        'en': "<strong>$<em>250</em></strong><p>Starting price</p>"
+    }[lang]);
+    $('#item2li').html({
+        'es': "" +
+        "<li><i class=\"fa fa-circle\"></i> Autenticacion</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Redes sociales</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Posicionamiento online</li>",
+        'en': "" +
+        "<li><i class=\"fa fa-circle\"></i> Auth</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Social network</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Online positioning</li>",
+    }[lang]);
+    $('#item3').html({
+        'es': "Sitio web",
+        'en': "Full Web Site"
+    }[lang]);
+    $('#item3d').html({
+        'es': "Sitio de gestion empresarial con soporte para multiples entidades en el negocio",
+        'en': "Management site with multiple business entities support"
+    }[lang]);
+    $('#item3p').html({
+        'es': "<strong>$<em>300</em></strong><p>Precio inicial</p>",
+        'en': "<strong>$<em>300</em></strong><p>Starting price</p>"
+    }[lang]);
+    $('#item3li').html({
+        'es': "" +
+        "<li><i class=\"fa fa-circle\"></i> Autenticacion</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Redes sociales</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Posicionamiento online</li>",
+        'en': "" +
+        "<li><i class=\"fa fa-circle\"></i> Auth</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Social network</li>" +
+        "<li><i class=\"fa fa-circle\"></i> Online positioning</li>",
+    }[lang]);
+    $('#text41').html({
+        'es': "Testimonios",
+        'en': "Customer testimonials"
+    }[lang]);
+    $('#text42').html({
+        'es': "Nuestros clientes opinan sobre el servicio",
+        'en': "Our customers opinions about our services"
+    }[lang]);
+    $('#text43').html({
+        'es': "Es impresionante la estabilidad y fluidez del trabajo con U&S, sin dudas los contratare de nuevo",
+        'en': "Is awesome the stable services, this guys from U&S are providing, i would hire them again"
+    }[lang]);
+    $('#text44').html({
+        'es': "Jerry Miller",
+        'en': "Jerry Miller"
+    }[lang]);
+    $('#text45').html({
+        'es': "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptownaliqua, retro synth master cleanse.",
+        'en': "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptownaliqua, retro synth master cleanse."
+    }[lang]);
+    $('#text46').html({
+        'es': "Albert Sparruena",
+        'en': "Albert Sparruena"
+    }[lang]);
+    $('#text47').html({
+        'es': "Williamsburg carles vegan helvetica. Cosby sweater eu banh mi, qui irure terry richardson ex squid Aliquip placeat salvia cillum iphone.",
+        'en': "Williamsburg carles vegan helvetica. Cosby sweater eu banh mi, qui irure terry richardson ex squid Aliquip placeat salvia cillum iphone."
+    }[lang]);
+    $('#text48').html({
+        'es': "Linda Adams",
+        'en': "Linda Adams"
+    }[lang]);
+    $('#text49').html({
+        'es': "Sobre nosotros",
+        'en': "About us"
+    }[lang]);
+    $('#text50').html({
+        'es': "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam sit nonummy nibh euismod tincidunt ut laoreet dolore magna aliquarm erat sit volutpat. Nostrud exerci tation ullamcorper suscipit lobortis nisl aliquip commodo consequat.",
+        'en': "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam sit nonummy nibh euismod tincidunt ut laoreet dolore magna aliquarm erat sit volutpat. Nostrud exerci tation ullamcorper suscipit lobortis nisl aliquip commodo consequat."
+    }[lang]);
+}
